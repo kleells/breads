45491f9af3d0ac03740cbc37bc0e4d348029e013
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-11 20:51:36 -0400

    added controllers and models route

---
+++ controllers/breads_controller.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bread = require('../models/bread.js')
+const breads = express.Router()
+
+
+// INDEX
+breads.get('/', (req, res) => {
+  res.send(bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  res.send(bread[req.params.arrayIndex])
+})
+
+module.exports = breads
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-
 // DEPENDENCIES
 const express = require('express');
-const { append } = require('express/lib/response');
 
 // CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
-console.log(PORT);
+const app = express();
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!');
-})
-
+    res.send('Welcome to an Awesome App about Breads');
+  });
+  
+// Breads
+const breadsController = require('./controllers/breads_controller.js');
+app.use('/breads', breadsController);
+  
 // LISTEN
 app.listen(PORT, () => {
     console.log('nomming at post', PORT);
-})
+});
