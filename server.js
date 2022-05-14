f0ae607444954109f6302fb40c7be6135be6cc23
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-14 09:05:43 -0400

    added some navigation buttons and new route styling

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT;
 const app = express();
 
 // MIDDLEWARE
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -19,6 +22,11 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js');
 app.use('/breads', breadsController);
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
   
 // LISTEN
 app.listen(PORT, () => {
--- views/index.jsx
+++ views/index.jsx
@@ -4,22 +4,22 @@ const Default = require('./layouts/Default')
 function Index ({breads, title})  {
     return (
       <Default title={title}>
-  <h2>Index Page</h2>
-    {/* This is a JSX comment. */}
-    {/* <p>I have {breads[0].name} bread!</p> */}
-    <ul>
-      {
-        breads.map((breads, index)=> {
-          return (<li key={index}>
-            <a href={`/breads/${index}`}>
-              {breads.name}
-            </a>
-          </li>)
-        })
-      }
-    </ul>
-</Default>
-
+        <h2>Index Page</h2>
+        <ul>
+          {
+            breads.map((breads, index)=> {
+              return (<li key={index}>
+                <a href={`/breads/${index}`}>
+                  {breads.name}
+                </a>
+              </li>)
+            })
+          }
+        </ul>
+        <div className="newButton">
+          <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+      </Default>
     )
 }
 
