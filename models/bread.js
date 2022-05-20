9804d9c3f99d7f3e826167e2e6c4a834dfd16088
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-20 18:06:34 -0400

    Added Bakers

--- models/bread.js
+++ models/bread.js
@@ -4,7 +4,11 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'https://unsplash.com/photos/rsWZ-P9FbQ4' }
+  image: { type: String, default: 'https://unsplash.com/photos/rsWZ-P9FbQ4' },
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Pheobe']
+  }
 })
 
 const bread = mongoose.model('Bread', breadSchema)
