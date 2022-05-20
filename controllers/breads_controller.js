f42db587ea5c68625f0fa7580719e5986f4f1574
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-20 17:16:41 -0400

    incorped Helper Methods) cler

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { findOneAndUpdate } = require('../models/bread')
 const bread = require('../models/bread')
 const breads = express.Router()
 
@@ -32,6 +33,34 @@ breads.get('/new', (req, res) => {
   res.render('New')
 })
 
+breads.get('/data/seed', (req, res) => {
+  bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ])
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
 // SHOW
 breads.get('/:id', (req, res) => {
   bread.findById(req.params.id)
@@ -46,28 +75,36 @@ breads.get('/:id', (req, res) => {
 })
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+//breads.put('/:id, (req, res) = > {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  bread.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(updatedBread => {
+      console.log(updatedBread)
+      res.redirect(`/breads/${req.params.id}`)
+    })
 })
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+breads.get('/:id/edit', (req, res) => {
+  bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
 })
 
 module.exports = breads
--- models/bread.js
+++ models/bread.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'http://placehold.it/500x500.png' }
+  image: { type: String, default: 'https://unsplash.com/photos/rsWZ-P9FbQ4' }
 })
 
 const bread = mongoose.model('Bread', breadSchema)
